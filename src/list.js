03cc20fd26eb25c317008d64436fbc73f7ae5e39
Lochan patra <143163989+lochanpatra@users.noreply.github.com>
2024-12-18 18:11:45 +0530

    Update list.js

--- src/list.js
+++ src/list.js
@@ -1,13 +1,52 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// @flow
 
-import App from "./App";
+import React from 'react';
+import { connect } from 'react-redux';
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
 
+function Items(props) {
+
+  return (
+    <div>
+      <div>
+      <input size="50" value = {props.text} onChange={props.numberChange} />
+      <button onClick={props.onSort}> SORT </button>
+      </div>
+      <p></p>
+      <div>
+      <input size="50" value = {props.text} onChange={props.numberChange} />
+      <button onClick={props.onReverse}> Reverse </button>
+      </div>
+  </div>
+  
+  )
+}
+
+const mapStateToProps= (state) =>{
+  return{
+    text: state.text
+  }
+}
+
+
+function mapDispatchToProps(dispatch)
+{
+  return{
+    onSort: (evt)=>{
+      const action={type: 'SORT', text: evt.target.value};
+      dispatch(action);
+    },
+    onReverse: (evt)=>{
+      const action={type: 'REVERSE', text: evt.target.value};
+      dispatch(action);
+    },
+    numberChange: (evt)=>{
+      const action={type: 'INPUT_CHANGE', text: evt.target.value};
+      dispatch(action);
+    }
+
+    
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Items);
